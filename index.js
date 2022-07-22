638a493606ab7159c464d908c5ba272ec55e5c9f
Christian A <christiana@iMac-de-Christian.local>
2022-07-22 16:06:50 -0500

    manejo de rutas con express

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'producto 1',
+    price: 1000
+  });
+});
+
 app.listen(port, () => {
   console.log(`aplicacion corriendo en el puerto http://localhost${port}`);
 });
