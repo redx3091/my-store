b4cdfcb4cccc231ca7b5b484a34ab7c5515d542b
Christian A <christiana@iMac-de-Christian.local>
2022-07-28 17:15:07 -0500

    se agrego capa de servicios para manejo independiente de metodos

--- routes/produtcs.router.js
+++ routes/produtcs.router.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const faker = require('faker');
+const ProductService = require('../services/product.service');
 
 const router = express.Router();
+const service = new ProductService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const {size} = req.query;
-  const limit = size || 10;
-  for(let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    });
-  }
+  const products = service.find();
   res.json(products);
 });
 
@@ -23,17 +15,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   let { id } = req.params;
-  if(id === '999') {
-    res.status(404).json({
-      message: 'no existe ese producto',
-    });
-  }  else {
-    res.status(200).json({
-      id: id,
-      name: 'producto 1',
-      price: 1000
-    });
-  }
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 router.post('/', (req, res) => {
