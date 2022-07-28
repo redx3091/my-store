ae525c6b0050cff65945faa8e964aaf0531297ed
Christian A <christiana@iMac-de-Christian.local>
2022-07-28 12:32:37 -0500

    moviendo el codigo las rutas a sus archivos independientes de rutas

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const faker = require('faker');
+const routerApi = require('./routes/router');
+
+
 const app = express();
 const port = 3000;
+
+routerApi(app);
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -11,109 +15,6 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const {size} = req.query;
-  const limit = size || 10;
-  for(let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    })
-  }
-  res.json(products);
-});
-
-
-app.get('/users', (req, res) => {
-  const {limit, offset} = req.query;
-  if(limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('No hay parametros')
-  }
-})
-
-app.get('/products/filter', (req, res) => {
-  res.send('yo sou un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id: id,
-    name: 'producto 1',
-    price: 1000
-  });
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
-
-app.get('/categories', (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: 'Home',
-    },
-    {
-      id: 2,
-      name: 'kitchen',
-    },
-    {
-      id: 3,
-      name: 'bathroom',
-    },{
-      id: 4,
-      name: 'restroom',
-    }
-  ]);
-});
-
-app.get('/categories/:id', (req, res) => {
-  const {id} = req.params;
-  res.json({
-    id: id,
-    name: 'Home'
-  });
-})
-
-app.get('/people', (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: 'juan',
-    },
-    {
-      id: 2,
-      name: 'andres',
-    },
-    {
-      id: 3,
-      name: 'carlos',
-    },{
-      id: 4,
-      name: 'ana',
-    }
-  ]);
-});
-
-app.get('/people/:id', (req, res) => {
-  const {id} = req.params;
-  res.json({
-    id: id,
-    name: 'Ana'
-  });
-})
 app.listen(port, () => {
-  console.log(`aplicacion corriendo en el puerto http://localhost${port}`);
+  console.log(`aplicacion corriendo en el puerto http://localhost:${port}`);
 });
