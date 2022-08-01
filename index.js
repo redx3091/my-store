e1a7a95138b7da6bd1ef2f4a0555627e962ef77c
Christian A <christiana@iMac-de-Christian.local>
2022-08-01 15:17:08 -0500

    desploy en heroku

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ const options = {
 }
 app.use(cors(options));
 
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+// app.get('/nueva-ruta', (req, res) => {
+//   res.send('Hola, soy una nueva ruta');
+// });
+
 routerApi(app);
 
 app.use(logErrors);
