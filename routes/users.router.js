688149e39179684e29c16f2228f6d519446a5f5d
Christian A <christiana@iMac-de-Christian.local>
2022-08-01 16:43:25 -0500

    edicion de los archivos de usuarios service,router y schema

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,38 +1,73 @@
 const express = require('express');
-const UserService = require('../services/users.service');
-//const faker = require('faker');
+
+const UserService = require('./../services/user.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { updateUserSchema, createUserSchema, getUserSchema } = require('./../schemas/user.schema');
 
 const router = express.Router();
 const service = new UserService();
 
-router.get('/', (req, res) => {
-  const users = service.find();
-  res.json(users);
+router.get('/', async (req, res, next) => {
+  try {
+    const categories = await service.find();
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  const {id} = req.params;
-  const user = service.findOne(id);
-  res.json(user);
-})
+router.get('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  const newUser = service.create(body);
-  res.status(201).json({newUser});
-});
+router.post('/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.patch('/:id', (req, res) => {
-  const {id} = req.params;
-  const body = req.body;
-  const user = service.update(id, body);
-  res.json(user);
-})
+router.patch('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.delete('/:id', (req, res) => {
-  const {id} = req.params;
-  const response = service.delete(id)
-  res.json(response)
-});
+router.delete('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
--- services/users.service.js
+++ services/users.service.js
@@ -1,59 +1,29 @@
-const faker = require('faker')
+const boom = require('@hapi/boom');
+
 class UserService {
-  constructor() {
-    this.users = [];
-    this.generate();
+  constructor() {}
+
+  async create(data) {
+    return data;
   }
 
-  generate() {
-    const limit = 100;
-    for(let i = 0; i < limit; i++) {
-    this.users.push({
-      id: faker.datatype.uuid(),
-      name: faker.name.firstName()
-    });
-  }
+  async find() {
+    return [];
   }
 
-  create(data) {
-    const newUser = {
-      id: faker.datatype.uuid(),
-      ...data
-    }
-    this.users.push(newUser);
-    return newUser;
+  async findOne(id) {
+    return { id };
   }
 
-  find() {
-    return this.users;
-  }
-
-  findOne(id) {
-    return this.users.find(item => item.id === id);
-  }
-
-  update(id, changes) {
-    const index = this.users.findIndex(item => item.id === id);
-
-    if(index === -1){
-      throw new Error('user not found');
-    }
-    const user = this.users[index];
-    this.users[index] = {
-      ...user,
-      ...changes
+  async update(id, changes) {
+    return {
+      id,
+      changes,
     };
-    return this.users[index];
   }
 
-  delete(id) {
-    const index = this.users.findIndex(item => item.id === id);
-
-    if(index === -1){
-      throw new Error('Product not found');
-    }
-    this.users.splice(index, 1);
-    return {id};
+  async delete(id) {
+    return { id };
   }
 }
 
