5ef1b76755781b7dc9f1f61a5937b99500347242
Christian A <christiana@iMac-de-Christian.local>
2022-08-01 11:28:35 -0500

    usando los middleware creados con joi para validacion de datos

--- routes/produtcs.router.js
+++ routes/produtcs.router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const ProductService = require('../services/product.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('../schemas/product.schema');
 
 const router = express.Router();
 const service = new ProductService();
@@ -9,7 +11,7 @@ router.get('/', async (req, res) => {
   res.json(products);
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', validatorHandler(getProductSchema, 'params'), async (req, res, next) => {
   try {
     let { id } = req.params;
     const product = await service.findOne(id);
@@ -19,13 +21,13 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', validatorHandler(createProductSchema, 'body'), async (req, res) => {
   const body = req.body;
   const newProduct = await service.create(body);
   res.status(201).json({newProduct});
 });
 
-router.patch('/:id', async (req, res, next) => {
+router.patch('/:id', validatorHandler(getProductSchema, 'params'), validatorHandler(updateProductSchema, 'body'), async (req, res, next) => {
   try {
     const {id} = req.params;
     const body = req.body;
--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -1,17 +1,20 @@
 const joi = require('joi');
 
 const id = joi.string().uuid();
-const name = joi.string().alphanum().min(3).max(15);
+const name = joi.string().min(3).max(15);
 const price = joi.number().integer().min(10);
+const image = joi.string().uri();
 
 const createProductSchema = joi.object({
   name: name.required(),
   price: price.required(),
+  image: image.required(),
 });
 
 const updateProductSchema = joi.object({
   name: name,
   price: price,
+  image: image,
 });
 
 const getProductSchema = joi.object({
