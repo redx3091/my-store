57cfa9f21c29f1e22d73cdd91f9a6b9407874854
Christian A <christiana@iMac-de-Christian.local>
2022-08-03 15:47:14 -0500

    creamos archivo config y archivo de variables de entorno para configurar y tener mejores practicas al suar las variables de entorno y no tener crredenciales de la DB en codigo

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,13 +1,11 @@
 const { Pool } = require('pg')
+const { config } = require('../config/config');
 
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-    const pool = new Pool({
-        host: 'localhost',
-        port: 5432,
-        user: 'store',
-        password: 'admin123',
-        database: 'my_store'
-    });
+    const pool = new Pool({ connectionString: URI });
  
 
 module.exports = pool;
