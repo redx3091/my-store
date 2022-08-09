2dc4c7012cfb48a6680f4033b7e7ee9d86ea2d43
Christian A <christiana@iMac-de-Christian.local>
2022-08-09 10:11:42 -0500

    FIX problema conexion base de datos

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,11 +5,11 @@ const setupModels = require('../db/models/index');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
-  logging: true,
+  dialect: 'postgres',
+  //logging: true,
 });
 
 setupModels(sequelize);
